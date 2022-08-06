1503074655e78a16a37b2ee1670e623c424ca14b
ArsazuZavala <zavalajoshua@gmail.com>
2022-08-06 00:20:13 -0500

    Tarea 1 Realizada

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,31 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let result = 0;
+  for (let i = 0; i < num.length; i++) {
+    result += num[i] * (Math.pow(2, num.length - 1 - i));
+  }
+  return result;
 }
 
-function DecimalABinario(num) {
-  // tu codigo aca
+const BinarioADecimal2 = (num) => {
+  let arr = num.split('');
+  let decimal = arr.reduce((acc, ele, index) => {
+    return acc + Number(ele) * Math.pow(2, (num.length - 1 - index));
+  },0)
+  return decimal;
+}
 
+const DecimalABinario = (num) => {
+  // tu codigo aca
+  let acc = num;
+  let binary = '';
+  while(acc > 0){
+    let residuo = acc % 2;
+    binary += residuo;
+    acc = Math.floor(acc / 2);
+  }
+  return binary.split('').reverse().join('');
 }
 
 

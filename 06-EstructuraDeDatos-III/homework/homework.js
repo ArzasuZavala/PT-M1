787edf5f7c2f6063f2909b081c93de4518703a07
ArsazuZavala <zavalajoshua@gmail.com>
2022-08-21 18:41:37 -0500

    Tarea 6 Realizada

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -11,8 +11,76 @@
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
 
-function BinarySearchTree() {}
-
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+  size() {
+    if (!this.left && !this.right) return 1;
+    if (this.left && this.right)
+      return 1 + this.left.size() + this.right.size();
+    if (this.left) return 1 + this.left.size();
+    if (this.right) return 1 + this.right.size();
+  }
+  insert(info) {
+    let subTree = new BinarySearchTree(info);
+    if (info < this.value) {
+      if (!this.left) this.left = subTree;
+      else this.left.insert(info);
+    } else {
+      if (!this.right) this.right = subTree;
+      else this.right.insert(info);
+    }
+  }
+  contains(search) {
+    if (this.value === search) return true;
+    if (search < this.value) {
+      if (!this.left) return false;
+      else return this.left.contains(search);
+    } else {
+      if (!this.right) return false;
+      else return this.right.contains(search);
+    }
+  }
+  depthFirstForEach(cb, texto) {
+    if (!texto || texto === "in-order") {
+      if (this.left) {
+        this.left.depthFirstForEach(cb, texto);
+      }
+      cb(this.value);
+      if (this.right) {
+        this.right.depthFirstForEach(cb, texto);
+      }
+    } else if (texto === "pre-order") {
+      cb(this.value);
+      if (this.left) {
+        this.left.depthFirstForEach(cb, texto);
+      }
+      if (this.right) {
+        this.right.depthFirstForEach(cb, texto);
+      }
+    } else if (texto === "post-order") {
+      if (this.left) {
+        this.left.depthFirstForEach(cb, texto);
+      }
+      if (this.right) {
+        this.right.depthFirstForEach(cb, texto);
+      }
+      cb(this.value);
+    }
+  }
+  breadthFirstForEach(cb, array) {
+    if (!array) array = [];
+    if (this.left) array.push(this.left);
+    if (this.right) array.push(this.right);
+    cb(this.value);
+    if (array.length > 0) {
+      array.shift().breadthFirstForEach(cb, array);
+    }
+  }
+}
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 

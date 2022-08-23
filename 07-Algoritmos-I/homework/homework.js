eac024d8823de18d25d2dab87a46a5dc82a620d2
ArsazuZavala <zavalajoshua@gmail.com>
2022-08-23 11:41:53 -0500

    Tarea 7 Realizada

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -1,40 +1,80 @@
-'use strict'
+"use strict";
 // No cambies los nombres de las funciones.
 
-function factorear(num) {
+const factorear = (num) => {
   // Factorear el número recibido como parámetro y devolver en un array
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
-}
-
-function bubbleSort(array) {
+  let result = [1];
+  let i = 2;
+  let number = num;
+  while (number !== 1) {
+    if (number % i === 0) {
+      result.push(i);
+      number = number / i;
+    } else {
+      i++;
+    }
+  }
+  return result;
+};
+const bubbleSort = (array) => {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
-}
-
-
-function insertionSort(array) {
+  let swap = true;
+  while (swap) {
+    swap = false;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > array[i + 1]) {
+        let aux = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = aux;
+        swap = true;
+      }
+    }
+  }
+  return array;
+};
+const insertionSort = (array) => {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for (let i = 1; i < array.length; i++) {
+    let j = i - 1;
+    let aux = array[i];
+    while (j >= 0 && aux < array[j]) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = aux;
+  }
+  return array;
+};
 
-}
-
-
-function selectionSort(array) {
+const selectionSort = (array) => {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
-}
-
+  for (let i = 0; i < array.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[min] > array[j]) {
+        min = j;
+      }
+    }
+    if (i !== min) {
+      let aux = array[i];
+      array[i] = array[min];
+      array[min] = aux;
+    }
+  }
+  return array;
+};
 
 // No modificar nada debajo de esta línea
 // --------------------------------
